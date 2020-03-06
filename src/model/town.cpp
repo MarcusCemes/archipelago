0e12688522c7dab614d0cb1044becdec22175e9e
Marcus Cemes <20288819+MarcusCemes@users.noreply.github.com>
2020-03-06 17:25:35 +0100

    fix: compilation fixing

--- src/model/town.cpp
+++ src/model/town.cpp
@@ -11,46 +11,42 @@
 #include "town.hpp"
 
 using namespace town;
+using namespace std;
+using namespace node;
 
-/* === DEFINITIONS === */
+/* === INTERNAL DEFINITIONS AND PROTOTYPES === */
 
-/** The last set node/link creation type */
-enum Type { T_NULL, T_HOUSING, T_TRANSPORT, T_PRODUCTION, T_LINK };
-
-// char[] constants for changing the creation type, as contexpr strings aren't
-// allowed
-constexpr char *K_HOUSING("nbNodeL");
-constexpr char *K_TRANSPORT("nbNodTL");
-constexpr char *K_PRODUCTION("nbNodeL");
-constexpr char *K_LINK("nbLink");
-
-constexpr char COMMENT_DELIMITER('#');
-
-constexpr char* NUMBER_CHAR("0123456789");
-constexpr char NULL_CHAR('\0');
-
-/* === INTERNAL PROTOTYPES === */
 
 namespace {
-void parseTown(std::istream &stream);
-bool readLine(std::istream &stream, std::string &line);
-void parseLine(std::string &line, std::vector<node::Node> &nodes,
-               Type &selectedType);
-void readWords(std::istream &stream, std::vector<std::string> &words);
-void createType(std::vector<node::Node> &nodes,
-                    std::vector<std::string> &words, Type &selectedType);
-bool startsWith(std::string &string, char *item);
+/** Last selected type to be created */
+enum SelectedType { T_NULL, T_HOUSING, T_TRANSPORT, T_PRODUCTION, T_LINK };
+
+constexpr char COMMENT_DELIMITER('#');
+constexpr char NULL_CHAR('\0');
+
+void parseTown(istream &stream);
+bool readLine(istream &stream, string &line);
+void parseLine(string &line, vector<Node> &nodes, vector<Link> &links,
+               SelectedType &selectedType);
+void readWords(istream &stream, vector<string> &words);
+void parseNode(vector<Node> &nodes, vector<string> &words,
+               SelectedType &selectedType);
+void parseLink(vector<Link> &links, vector<string> &words);
+string extractKey(string &word);
+string extractValue(string &word);
+bool startsWith(string &sample, string &term);
+int getFirstNumberIndex(string &word);
 }  // namespace
 
 /* === FUNCTIONS === */
 
 /** Load and parse a town from a file */
 void loadFromFile(char *path) {
-  std::ifstream file(path);
+  ifstream file(path);
   if (file.is_open()) {
     parseTown(file);
   } else {
-    std::cerr << "File not open" << std::endl;
+    cerr << "File not open" << endl;
   }
 }
 
@@ -58,14 +54,15 @@ void loadFromFile(char *path) {
 
 namespace {
 /** Parses a town from a multiline input stream */
-void parseTown(std::istream &stream) {
-  std::vector<node::Node> nodes;
-  std::string line;
-  Type selectedType(T_NULL); // no type set yet
+void parseTown(istream &stream) {
+  vector<Node> nodes;
+  vector<Link> links;
+  string line;
+  SelectedType selectedType(T_NULL);  // no type set yet
 
   // Read each line and parse it
   while (readLine(stream, line)) {
-    parseLine(line, nodes, selectedType);
+    parseLine(line, nodes, links, selectedType);
   }
 }
 
@@ -73,13 +70,13 @@ void parseTown(std::istream &stream) {
  * Read a single line from a stream up to a comment delimiter
  * @returns `true` if a line was read, `false` otherwise
  */
-bool readLine(std::istream &stream, std::string &line) {
+bool readLine(istream &stream, string &line) {
   if (stream.eof()) return false;
 
-  std::string rawLine;
-  std::getline(stream, rawLine);
+  string rawLine;
+  getline(stream, rawLine);
   size_t commentPos(rawLine.find(COMMENT_DELIMITER));
-  if (commentPos == std::string::npos) {
+  if (commentPos == string::npos) {
     line = rawLine;
   } else {
     line = rawLine.substr(0, commentPos);
@@ -88,102 +85,87 @@ bool readLine(std::istream &stream, std::string &line) {
 }
 
 /** Parse a line of the town format */
-void parseLine(std::string &line, std::vector<node::Node> &nodes,
-               Type &selectedType) {
-  std::stringstream stream(line);
-  std::vector<std::string> words;
+void parseLine(string &line, vector<Node> &nodes, vector<Link> &links,
+               SelectedType &type) {
+  stringstream stream(line);
+  vector<string> words;
 
-  readWords(stream, words); // split string into words
+  readWords(stream, words);  // split string into words
   if (words.size() == 0) return;
 
   // Change the type selection
-  if (startsWith(words[0], K_HOUSING)) {
-    selectedType = T_HOUSING;
-  } else if (startsWith(words[0], K_HOUSING)) {
-    selectedType = T_TRANSPORT;
-  } else if (startsWith(words[0], K_PRODUCTION)) {
-    selectedType = T_PRODUCTION;
-  } else if (startsWith(words[0], K_LINK)) {
-    selectedType = T_LINK;
+  if (startsWith(words[0], "nbNodeH")) {
+    type = T_HOUSING;
+  // } else if (startsWith(words[0], "nbNodeT")) {
+  //   type = T_TRANSPORT;
+  // } else if (startsWith(words[0], "nbNodeP")) {
+  //   type = T_PRODUCTION;
+  // } else if (startsWith(words[0], "nbLink")) {
+  //   type = T_LINK;
   } else {
     // create the selected type
-    createType(nodes, words, selectedType);
+    if (type == T_LINK) {
+      parseLink(links, words);
+    } else {
+      parseNode(nodes, words, type);
+    }
   }
 }
 
 /** Splits all space-separated words and replace vector<string>'s contents */
-void readWords(std::istream &stream, std::vector<std::string> &words) {
+void readWords(istream &stream, vector<string> &words) {
   words.clear();
-  std::string word;
+  string word;
 
   while (stream >> word) {
     words.push_back(word);
   }
 }
 
-void createType(std::vector<node::Node> &nodes,
-                    std::vector<std::string> &words, Type &selectedType) {
-  std::vector<int> uids;
-  int pop;
-  double x, y;
+void parseNode(vector<Node> &nodes, vector<string> &words,
+               SelectedType &selectedType) {
+  int uid(stoi(extractValue(words[0])));
+  double x(stoi(extractValue(words[1])));
+  double y(stoi(extractValue(words[2])));
+  int pop(stoi(extractValue(words[3])));
 
-  // Decode each keyword
-  for (auto const &word : words) {
-    if (word >= "uid") {
-      uids.push_back(std::stoi(word.substr(3)));
-    } else if (word >= "x") {
-      x = std::stoi(word.substr(1));
-    } else if (word >= "y") {
-      y = std::stoi(word.substr(1));
-    } else if (word >= "nbp") {
-      pop = std::stoi(word.substr(3));
-    } else {
-      // TODO invalid key
-    }
-  }
+  NodeType type = selectedType == T_HOUSING
+                      ? HOUSING
+                      : selectedType == T_TRANSPORT ? TRANSPORT : PRODUCTION;
 
-  switch (selectedType) {
-    case (T_HOUSING):
-      nodes.push_back(node::Housing(uids.at(0), {x, y}, pop));
-      break;
-    case (T_TRANSPORT):
-      nodes.push_back(node::Transport(uids.at(0), {x, y}, pop));
-      break;
-    case (T_PRODUCTION):
-      nodes.push_back(node::Production(uids.at(0), {x, y}, pop));
-      break;
-    case (T_LINK):
-      // TODO create link
-      break;
-    default:
-      // TODO error
-  }
+  nodes.push_back(Node(type, uid, {x, y}, pop));
 }
 
-void splitKeyValue(std::string &string, std::string &key, std::string &value) {
+/** Parse a link creation line and add to the links vector */
+void parseLink(vector<Link> &links, vector<string> &words) {
+  int uid1(stoi(extractValue(words[0])));
+  int uid2(stoi(extractValue(words[1])));
 
+  links.push_back({uid1, uid2});
 }
 
-/** Check if string starts with character array */
-bool startsWith(std::string &string, char *item) {
-  size_t stringLength(string.size());
-  size_t itemLength(strlen(item));
-
-  if (stringLength < itemLength) return false;
-
-  for (size_t i(0); i < itemLength; ++i) {
-    if (item[i] != string[i]) return false;
-  }
-
-  return true;
+string extractKey(string &word) {
+  int index(getFirstNumberIndex(word));
+  return word.substr(0, index);
 }
 
-/** Returns true if a character is a number, false otherwise */
-bool isNumber(char &character) {
-  for (size_t i(0); NUMBER_CHAR[i] != NULL_CHAR; ++i) {
-    if (character == i[0]) return true;
+string extractValue(string &word) {
+  int index(getFirstNumberIndex(word));
+  return word.substr(index);
+}
+
+/** Get the index of the first numerical charcater in a string */
+int getFirstNumberIndex(string &word) {
+  size_t END(word.length());
+  for (size_t i(0); i < END; ++i) {
+    if (isdigit(word[i])) return i;
   }
-  return false;
+  return -1;
+}
+
+/** Check if a sample string starts with a character array */
+bool startsWith(string sample, string term) {
+  return sample > term;
 }
 
 }  // namespace
--- src/projet.cpp
+++ src/projet.cpp
@@ -3,9 +3,25 @@
 
 #include <iostream>
 
+#include "model/town.hpp"
+
 using namespace std;
 
-int main() {
-  cout << "Hello world!" << endl;
+
+/* === PROTOTYPES === */
+
+void init(char *file);
+
+
+/* === PROGRAM === */
+
+int main(int argc, char *argv[]) {
+  if (argc != 1) {
+    cerr << "No file input specified" << endl;
+    cerr << "Usage: " << argv[0] << " path/to/town.txt" << endl;
+    return 1;
+  }
+
+  town::loadFromFile(argv[1]);
   return 0;
 }
