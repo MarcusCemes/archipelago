e5b6ced46242cb4222277ebd27617852c299dffb
Marcus Cemes <20288819+MarcusCemes@users.noreply.github.com>
2020-03-06 16:49:13 +0100

    feat: basic Town class

--- src/model/town.hpp
+++ src/model/town.hpp
@@ -1,14 +1,33 @@
+
 // archipelago - model/town.hpp
 // Town-related logic, such as ENJ, CI and MTA calculations
 
 #ifndef MODEL_TOWN_H
 #define MODEL_TOWN_H
 
+#include <vector>
+
+#include "node.hpp"
+
 namespace town {
 
-// TODO
+/** A class that represents a Town */
+class Town {
+ public:
+  /* Accessors/Manipulators */
 
-}
+  std::vector<node::Node>& getNodes();
+  void setNodes(std::vector<node::Node>& nodes);
 
+  std::vector<node::Link>& getLinks();
+  void setLinks(std::vector<node::Link>& links);
+
+ private:
+  /* Attributes */
+  std::vector<node::Node> nodes;
+  std::vector<node::Link> links;
+};
+
+}  // namespace town
 
 #endif
