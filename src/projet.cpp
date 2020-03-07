114d0fde209c3b5e7f1ba0f2108a2260344f5d1d
Marcus Cemes <20288819+MarcusCemes@users.noreply.github.com>
2020-03-07 11:49:04 +0100

    feat: Town class, town file load/parse functions (#8)

--- src/projet.cpp
+++ src/projet.cpp
@@ -3,9 +3,42 @@
 
 #include <iostream>
 
+#include "model/town.hpp"
+
 using namespace std;
 
-int main() {
-  cout << "Hello world!" << endl;
+/* === PROTOTYPES === */
+
+void init();
+void init(char *file);
+void start(town::Town &town);
+
+/* === PROGRAM === */
+
+int main(int argc, char *argv[]) {
+  if (argc >= 2) {
+    init(argv[1]);
+  } else {
+    init();
+  }
+
   return 0;
+}
+
+/** Launch the GUI with an empty town */
+void init() {
+  town::Town town;
+  start(town);
+}
+
+/** Create a town from saved file and launch the GUI */
+void init(char *file) {
+  town::Town town(town::loadFromFile(file));
+  // TODO validate town
+  start(town);
+}
+
+/** STUB - Launch the program with the passed town */
+void start(town::Town &town) {
+  town.getNodes();  // stub, hide -Wunusfed-parameter warning
 }
